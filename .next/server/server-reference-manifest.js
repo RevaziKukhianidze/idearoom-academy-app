910938fa78279a2b69e843882553e4c73fb8f8f5
Kukhianidze <revazi.kukhianidze@majorel.com>
2025-05-15 17:23:54 +0400

    deleted server.js

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"L4m7DamF/Jx0kpBWL3QrQKzUO9FJfdJtej19emIPZ9o=\"\n}"
