2329d2b29fa4bb6f6a6e64b0f634a8a22d25cf46
SabaGG71 <sgachechiladze881@gmail.com>
2025-06-12 23:17:10 +0400

    add cors

---
+++ supabase/functions/my-function/index.ts
@@ -0,0 +1,19 @@
+export const handler = async (req: Request): Promise<Response> => {
+  if (req.method === "OPTIONS") {
+    return new Response(null, {
+      status: 204,
+      headers: {
+        "Access-Control-Allow-Origin": "https://academy.idearoom.ge",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    });
+  }
+
+  return new Response(JSON.stringify({ message: "Worked with CORS!" }), {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "https://academy.idearoom.ge",
+    },
+  });
+};
